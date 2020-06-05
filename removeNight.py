d31ddc93e2a69a7ae736bd6d62244a2fcdcdc798
Aaron Nédélec <65923322+ReactiveAlkali@users.noreply.github.com>
2020-06-05 18:34:26 -0500

    Add files via upload

---
+++ removeNight.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+# This script removes all nighttime pictures from yesterday's image folder base on yesterday's sunrise and sunset
+
+import json
+import os
+import datetime
+from pathlib import Path
+
+TWILIGHT = 20  # The approximate duration of twilight in minutes
+
+# Gets the sunrise and sunset times for the given date
+def sunriseAndSet( date ):
+    # Gets the weather JSON
+    jsonFile = open("weather/" + date + "/23:00.json", 'r')
+    data     = json.load(jsonFile)
+
+    # Extract sunrise and sunset
+    sys     = data['sys']
+    sunrise = sys['sunrise']
+    sunset  = sys['sunset']
+
+    return sunrise,sunset
+
+# Calculates the start and end of daylight before converting to local time
+def convertLocal(sunrise, sunset, twilight):
+    # Calculates start and end of daylight
+    secTwlght = twilight * 60
+    dayStart  = sunrise - secTwlght
+    dayEnd    = sunset  + secTwlght
+
+    # Converts to local time
+    dayStart = (datetime.datetime.fromtimestamp(dayStart)).strftime('%H:%M')
+    dayEnd   = (datetime.datetime.fromtimestamp(dayEnd)).strftime('%H:%M')
+
+    return dayStart,dayEnd
+
+
+# Removes all images taken during night time
+def removeNight(dayStart, dayEnd, date):
+    dirPath = "images/" + date
+
+    # iterate through image directory
+    for filename in os.listdir(dirPath):
+        if filename.endswith(".jpg"):
+            time = filename.strip('.jpg')
+            
+            # remove if night
+            night = (time < dayStart) or (time > dayEnd)
+            if night:
+                path = dirPath + '/' + filename
+                os.remove(path)
+        
+
+def main():
+    yesterday = (datetime.datetime.today() - datetime.timedelta(1)).strftime('%Y-%m-%d')
+    
+    sunrise,sunset = sunriseAndSet(yesterday)
+    
+    dayStart,dayEnd = convertLocal(sunrise, sunset, TWILIGHT)
+    
+    removeNight(dayStart, dayEnd, yesterday) 
+
+
+if __name__ == "__main__":
+    main()
