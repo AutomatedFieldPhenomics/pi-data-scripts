f90ce8018043b66d3a0cd32b807511e3928fb754
Aaron Nedelec <aaronnedelec@pop-os.localdomain>
2020-08-06 16:20:49 -0500

    commit

--- lightlux.py
+++ lightlux.py
@@ -7,31 +7,43 @@ import os
 import datetime
 import csv
 
-csvFile = 'lightlux.csv'
-directory = '/home/pi/sensorData
-path = csvFile + '/' + directory
-
-# Create directory if one does not exist
-if not os.path.exists(directory):
-    os.makedirs(directory)
-
-# Take lux measurment
-tsl = tsl2591()
-full, ir = tsl.get_full_luminosity()
-lux = tsl.calculate_lux(full, ir)
-
-# Get date and time
-now = datetime.datetime.now()
-date = now.strftime("%Y-%m-%d")
-time = now.strftime("%H:%M:%S")
-
-# Create CSV header if one does not already exist
-firstRow = "Lux, Full, IR, Date (ymd), Time\n"
-if not os.access(path, os.F_OK):
-    with open(path, 'a') as fd:
-        fd.write(firstRow)
-
 # Append data to CSV
 currentRow = "{L},{F},{I},{D},{t}\n".format(L=lux, F=full, I=ir, D=date, t=time)
 with open(path, 'a') as fd:
     fd.write(currentRow)
+
+def getLux():
+    tsl = tsl2591()
+    full, ir = tsl.get_full_luminosity()
+    lux = tsl.calculate_lux(full, ir)
+    return lux
+
+def getFile(now):
+    csvFile = now.strftime("%Y-%m") + ".csv"
+    dir = '/home/pi/sensorData/tsl2591'
+    path = dir + '/' + csvFile
+
+    # Create directory if it doesn't exist
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+    # Create csv file if one doesn't exist
+    firstRow = 'Lux, Full, IR, Date (ymd), Time\n'
+    if not os.access(path, os.F_OK):
+        with open(path, 'a') as fd:
+            fd.write(firstRow)
+
+    return path
+
+def recordLux(lux, path):
+    current
+
+def main():
+    now = datetime.datetime.now()
+    lux = getLux()
+    path = getFile(now)
+
+
+
+if __name__ == "__main__":
+    main()
