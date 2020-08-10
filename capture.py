f8ed90390532cb3a6c8157f5e3cc13d4a5cf4d1d
Aaron Nedelec <aaronnedelec@pop-os.localdomain>
2020-08-10 17:54:42 -0500

    Removed use of light sensor

--- capture.py
+++ capture.py
@@ -1,46 +1,27 @@
 #!/usr/bin/env python3
 
-# This script captures images using picamera, additional code has been added to prevent taking images when the light levels are below a certain point
+# This script uses a Raspberry Pi camera module to capture an image
 
 from picamera import PiCamera
 from time import sleep
 import datetime
 import os
-import ts12591
 
-LUX_THRESHOLD = 1
+camera = PiCamera()
+camera.resolution = (720, 480)
+camera.rotation = 180
 
-def capture():
-    camera = PiCamera()
-    camera.resolution = (720, 480)
-    camera.rotation = 180
+# start preview seems to be necessary for white balance and auto exposure
+camera.start_preview()
+sleep(2)
 
-    # start preview seems to be necessary for white balance and auto exposure
-    camera.start_preview()
-    sleep(2)
+now = datetime.datetime.now()
 
-    now = datetime.datetime.now()
+dir = "images/%s" % now.strftime("%Y-%m-%d")
+if not os.path.exists(dir):
+    os.makedirs(dir)
 
-    dir = "images/%s" % now.strftime("%Y-%m-%d")
-    if not os.path.exists(dir):
-        os.makedirs(dir)
+camera.capture('%s/%s.png' % (dir, now.strftime("%H:%M:%S")))
 
-    camera.capture('%s/%s.png' % (dir, now.strftime("%H:%M:%S")))
+camera.stop_preview()
 
-    camera.stop_preview()
-    return
-
-def getLux():
-    tsl = tsl2591.Tsl2591()
-    full, ir = tsl.get_full_luminosity()
-    lux = tsl.calculate_lux(full, ir)
-    return lux
-
-def main():
-    lux = getLux()
-
-    if lux > LUX_THRESHHOLD:
-        capture()
-
-if __name__ == "__main__":
-    main()
